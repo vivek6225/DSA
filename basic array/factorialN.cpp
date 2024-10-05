d4b5ca8295261ed1ea26b199a618b91055dd31ae
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-10-05 16:35:32 +0530

    .

--- basic array/factorialN.cpp
+++ basic array/factorialN.cpp
@@ -1,2 +1,15 @@
-#include<bitsstdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+ int factorial(int n){
+  int fact = 1;
+  for(int i = 1; i<=n; i++){
+    fact = fact*i;
+  }
+  return fact;
+}
+int main(){
+  cout<<"factorial 4 = "<<factorial(4)<<endl;
+  cout<<"factorial 10 = "<<factorial(10);
+  return 0;
+
+}
