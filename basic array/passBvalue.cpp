563e81a5e21e6621c8e07a2ed4ab66f57d1ab4f6
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-10-05 16:59:00 +0530

    .

---
+++ basic array/passBvalue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void changeX(int x){
+  x =  2*x;
+  cout<<"x = "<< x <<endl;
+
+}
+int main(){
+  int x = 5;
+  changeX(x);
+  cout<<"X = "<< x <<endl;
+
+
+}
