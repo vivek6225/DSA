59fa92fba73432c8c7d347e48db51f287b4eb2aa
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-10-06 16:03:01 +0530

    .

--- ARRAY/printsubarray.cpp
+++ ARRAY/printsubarray.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 int main()
 {
-  int n = 6;
-  int arr[6] = {1,2,3,4,5,6};
+int arr[1000];
+int n;
+cout<<"enter the size of array  = ";
+cin>>n;
+cout<<"Enter the element = ";
+for(int i = 0; i<n; i++)
+cin>>arr[i];
+
   for(int start = 0; start<n; start++){
     for(int end = start; end<n; end++){
       for(int i = start; i<=end;i++){
