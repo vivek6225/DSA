0bb5f25bac6a726482ae508deaf4e3ee5a827e74
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-10-09 09:15:58 +0530

    .

---
+++ ARRAY/best time to buy and sell stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprofit = 0, bestBuy = prices[0];
+
+        for(int i = 1; i<prices.size();i++){
+            if(prices[i]>bestBuy){
+                maxprofit = max(maxprofit, prices[i] - bestBuy);
+            }
+                bestBuy = min(bestBuy, prices[i]);
+
+            }
+            return maxprofit;
+        
+    }
+};
