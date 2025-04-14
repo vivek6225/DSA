c77f3b68cd5e134e79c76598f0caa53ea3e768fc
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-04-14 13:15:23 +0530

    LT-46. Permutations

--- RECURSION/Permutations.cpp
+++ RECURSION/Permutations.cpp
@@ -1,30 +1,29 @@
-class Solution {
-  public:
-         
-         // t.c = 0(n!*n)
-         // s.s = 0(n!)
-   
-      void permut(vector<int>&arr,vector<vector<int> >&ans,int index)
-          {
-              if(arr.size()==index)
-              {
-                  ans.push_back(arr);
-                  return;            
-          }
-          for(int i=index; i<arr.size(); i++)
-          {
-              swap(arr[index],arr[i]);
-              permut(arr,ans,index+1);
-              swap(arr[index],arr[i]);
+// t.c = 0(n!*n)
+ // s.s = 0(n!)
+
+ class Solution {
+    public:
+        void permut(vector<int>&arr,vector<vector<int> >&ans,int index)
+            {
+                if(arr.size()==index) // base case
+                {
+                    ans.push_back(arr);
+                    return;            
+            }
+            for(int i=index; i<arr.size(); i++)
+            {
+                swap(arr[index],arr[i]);
+                permut(arr,ans,index+1);
+                swap(arr[index],arr[i]); // backtrack
+              
+            }
+            }
+        vector<vector<int>> permute(vector<int>& nums) {
             
-          }
-          }
-      vector<vector<int>> permute(vector<int>& nums) {
-          
-              vector<vector<int>>ans;
-             permut(nums,ans,0); 
-             return ans; 
-          
-          
-      }
-  };
+                vector<vector<int>>ans;
+               permut(nums,ans,0); 
+               return ans; 
+            
+            
+        }
+    };
