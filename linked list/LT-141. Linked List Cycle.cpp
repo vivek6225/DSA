4d9613653d2b527d1b4835d783c623e4b3498185
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-04-19 19:23:49 +0530

    LT-141. Linked List Cycle

---
+++ linked list/LT-141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+  public:
+      bool hasCycle(ListNode *head) {
+          ListNode *slow = head,*fast = head;
+          while(fast != NULL && fast->next != NULL){
+              slow = slow->next;
+              fast = fast->next->next;
+              if( slow == fast){
+                return true;  
+              }
+          }
+          return false;
+          }   
+  };
