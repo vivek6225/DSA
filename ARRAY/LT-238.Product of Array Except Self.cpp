a70e1fe2b9e3a55fd0f789d35d0dc8f30b52d0fb
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-08-04 23:57:12 +0530

    LT-238.product of Array Except itself

--- ARRAY/LT-238.Product of Array Except Self.cpp
+++ ARRAY/LT-238.Product of Array Except Self.cpp
@@ -1,3 +1,45 @@
+
+     // BRUTE  FORCE  APPROACH
+     //T.C = 0(N*N)
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>ans(n,1);
+//         for(int  i = 0; i<n; i++){
+//           for(int j = 0; j<n; j++){
+//             if(i != j){
+//                 ans[i] *= nums[j]; 
+           
+//             }
+//           } 
+//         }
+//        return ans;
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
