991d4d90ff7ffd696c5153d628e72ab52ecf2e8a
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-08-06 20:09:08 +0530

    print All Element in Queue

--- queue/printAllElementInQueue.cpp
+++ queue/printAllElementInQueue.cpp
@@ -1,31 +1,59 @@
-//s.c 0(n)
-//t,c= 0(n)
-// brute force
+//s.c =0(1)
+//t.c = 0(n)
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
- queue<int> q;
- q.push(31);
- q.push(33);
- q.push(4);
- q.push(44);
- q.push(41);
+  queue<int> q;
+  q.push(1);
+  q.push(10);
+  q.push(10);
+  q.push(20);
 
-  vector<int>ans;
-
-  while(!q.empty()){
+  // value print  karao
+  int n = q.size();
+  while(n--){
     cout<<q.front()<<" ";
-    ans.push_back(q.front());
-    q.pop();
-  }
-  cout<<endl;
-    cout << "Queue size after popping: " << q.size() << endl;
-
-  for(int i = 0; i<ans.size();i++){
-    q.push(ans[i]);
+    q.push(q.front());  // add the front element to the back
+    q.pop();           // remove the front element
     
   }
-    cout << "Queue size after restoring: " << q.size() << endl;
-  return 0;
-  }
+  cout<<endl;
+  cout<<"size of queue = "<<q.size()<<endl;
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+                 //s.c 0(n)
+                 //t,c= 0(n)
+                 // brute force
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//  queue<int> q;
+//  q.push(31);
+//  q.push(33);
+//  q.push(4);
+//  q.push(44);
+//  q.push(41);
+
+//   vector<int>ans;
+
+//   while(!q.empty()){
+//     cout<<q.front()<<" ";
+//     ans.push_back(q.front());
+//     q.pop();
+//   }
+  // cout<<endl;
+  //   cout << "Queue size after popping: " << q.size() << endl;
+
+  // for(int i = 0; i<ans.size();i++){
+  //   q.push(ans[i]);
+    
+  // }
+  //   cout << "Queue size after restoring: " << q.size() << endl;
+  // return 0;
+  // }
 
