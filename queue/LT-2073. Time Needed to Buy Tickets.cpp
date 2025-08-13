832a2b9fd19e8622b82d7eeccbcc64a0cc65d309
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-08-13 17:33:26 +0530

    LT-2073. Time Needed to Buy Tickets

--- queue/LT-2073. Time Needed to Buy Tickets.cpp
+++ queue/LT-2073. Time Needed to Buy Tickets.cpp
@@ -1,32 +1,60 @@
-// T.C: O(n × m)
-// S.C: O(n)
 
-class Solution {
+               // Optimal Approach
+    //    Time Complexity: O(n)
+    //      Space Complexity: O(1)
+     class Solution {
 public:
+   
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
-        queue<int> q;
         int n = tickets.size();
+        int time = 0; // Total time taken
         
-        // Store all people in queue
+        // Loop through each person
         for (int i = 0; i < n; i++) {
-            q.push(i);
-        }
-        
-        int time = 0;
-        
-        while (tickets[k] > 0) {
-            tickets[q.front()]--; // Decrease ticket count of the person at front. 
-
-            // Push back if tickets left.
-            if (tickets[q.front()]) {
-                q.push(q.front());
+            if (i <= k) {
+                // People before or at k can buy up to tickets[k] tickets
+                time += min(tickets[k], tickets[i]);
+            } else {
+                // People after k can buy at most tickets[k] - 1 tickets
+                time += min(tickets[k] - 1, tickets[i]);
             }
-
-            q.pop();  // Remove from front
-            time++;   // 1 second for each ticket sold.
         }
         
         return time;
     }
 };
+
+
+                   // BETTER APPROACH
+         //T.C = 0(n*n)
+         //S.C = 0(n)  
+
+// class Solution {
+// public:
+//        int timeRequiredToBuy(vector<int>& tickets, int k) {
+        // queue<int> q;
+        // int n = tickets.size();
+        
+        // // Store all people in queue
+        // for (int i = 0; i < n; i++) {
+        //     q.push(i);
+        // }
+        
+        // int time = 0;
+        
+        // while (tickets[k] > 0) {
+        //     tickets[q.front()]--; // Decrease ticket count of the person at front. 
+
+        //     // Push back if tickets left.
+        //     if (tickets[q.front()]) {
+        //         q.push(q.front());
+        //     }
+
+        //     q.pop();  // Remove from front
+        //     time++;   // 1 second for each ticket sold.
+        // }
+        
+        // return time;
+//     }
+// };
