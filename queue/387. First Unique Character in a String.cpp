b77478fae0cf1f7602e4abe4c80dc14e211ee53c
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-08-15 23:46:06 +0530

    LT-387. First Unique Character in a String

---
+++ queue/387. First Unique Character in a String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>m;
+        queue<int>Q;
+        for(int  i = 0; i<s.size(); i++){
+            if(m.find(s[i]) == m.end()){
+            Q.push(i);
+        }
+        m[s[i]]++;
+        while(Q.size() > 0 && m[s[Q.front()]] > 1){
+            Q.pop();
+        }
+        }
+        return Q.empty() ? -1 :Q.front();
+        
+    }
+};
