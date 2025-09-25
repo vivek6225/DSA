746e41540bc7c666b56223e86dceb48f6010f1cc
Vivek Kumar <vivekkumar06102002@gmail.com>
2025-09-25 18:42:05 +0530

    UPD : 25/09/2025

--- Sliding Window/LT-239. Sliding Window Maximum.cpp
+++ Sliding Window/LT-239. Sliding Window Maximum.cpp
@@ -1,6 +1,7 @@
-            // optimal approach
-    //T.C = 0(n)
-    //S.C = 0(n)
+                
+                      // Optimal Approach
+       //T.C = O(n)
+       //S.C = O(n)
     class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -12,15 +13,21 @@ public:
         if(d.empty())
         d.push_back(i);
         else{
+
+  // Remove all smaller elements from the back of deque
         while(!d.empty() && nums[i] > nums[d.back()])
            d.pop_back();
+      // Add current index to deque
            d.push_back(i);
         }
        }
+        // Process the rest of the elements
         for(int i = k-1; i<n; i++){
-            // remove smallest element from back
+   // remove smallest element from back
             while(!d.empty() && nums[i] > nums[d.back()])
                 d.pop_back();
+                
+            // Add current index to deque
                 d.push_back(i);
             // if front element is outside the window, pop it
             if(d.front() <= i-k)
@@ -31,8 +38,8 @@ public:
          return ans;
     }
 };
-//-----------------------------------------------------------------
-                 //brute force apprach
+//---------------------------------------------------------------------
+                       //Brute force Approach
         //t.c = 0(n*k)
         //s.c = 0(n)
 class Solution {
@@ -40,13 +47,18 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>ans;
         int n = nums.size();
+
+         // Iterate over each possible window starting index
         for(int i = 0; i<= n-k;i++){
-          int   total = INT_MIN;
+          int   total = INT_MIN;// Initialize the maximum value in the window as very small
+            
+            // Traverse the current window of size k
           for(int j = i; j < i+k; j++){
-            total  = max(total,nums[j]);
+            total  = max(total,nums[j]);// Update maximum element in the current window
           }
+             // Store the maximum of the current window
           ans.push_back(total);
         }
-        return ans;
+        return ans; // Return the list of maximums for all windows
     }
 };
