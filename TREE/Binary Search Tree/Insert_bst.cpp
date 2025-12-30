47ad9d132bf007aeeab2a8a9f9c9270cc3b3549a
vivek kumar <vivekkumar06102002@gmail.com>
2025-12-30 23:52:58 +0530

    30/12/2025

---
+++ TREE/Binary Search Tree/Insert_bst.cpp
@@ -0,0 +1,70 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+
+  int data;
+  Node *left,*right;
+
+  Node(int value){
+  data  = value;
+  left=right = NULL;
+  } 
+};
+  Node * insert(Node*root,int target){
+    if(!root){
+    Node * temp = new Node(target);
+    return temp;
+    }
+    if( target < root->data)
+      
+      root->left = insert(root->left,target);
+    else
+      root->right = insert(root->right,target);
+      return root; 
+
+  }
+  void inorder(Node*root){
+    if(!root)
+    return;
+    // left
+    inorder(root->left);
+
+    // node
+    cout<<root->data<<" ";
+
+    // right
+    inorder(root->right);
+  }
+  
+   bool search(Node* root,int target){
+    if(!root)
+    return 0 ;
+    
+    if(root->data == target)
+    return 1;
+
+    if(root-> data > target){
+      return search(root->left,target);
+    }else
+    return search(root->right,target);
+   }
+
+  int  main(){ 
+
+  int arr[] = {6,3,17,5,11,18,2,1 ,20,14};
+  Node *root = NULL;
+  for(int i = 0; i<10; i++)
+  root = insert(root,arr[i]);
+  
+  // search
+  cout<<endl;
+  cout<<search(root,101)<<endl;
+
+  //traverse
+
+  inorder(root);
+  
+}
