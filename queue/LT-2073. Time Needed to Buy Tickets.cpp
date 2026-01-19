642bd6359384f331f0b14923815023b1eb93aa08
vivek kumar <vivekkumar06102002@gmail.com>
2026-01-20 01:16:55 +0530

    UPD : 20/1/2026

--- queue/LT-2073. Time Needed to Buy Tickets.cpp
+++ queue/LT-2073. Time Needed to Buy Tickets.cpp
@@ -26,35 +26,35 @@ public:
 };
 
 
-                   // BETTER APPROACH
-         //T.C = 0(n*n)
-         //S.C = 0(n)  
+//--------------------BETTER APPROACH---------------------------------------------------
+//T.C = 0(n*n)
+ //S.C = 0(n)  
 
-// class Solution {
-// public:
-//        int timeRequiredToBuy(vector<int>& tickets, int k) {
-        // queue<int> q;
-        // int n = tickets.size();
+class Solution {
+public:
+       int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int> q;
+        int n = tickets.size();
         
-        // // Store all people in queue
-        // for (int i = 0; i < n; i++) {
-        //     q.push(i);
-        // }
+        // Store all people in queue
+        for (int i = 0; i < n; i++) {
+            q.push(i);
+        }
         
-        // int time = 0;
+        int time = 0;
         
-        // while (tickets[k] > 0) {
-        //     tickets[q.front()]--; // Decrease ticket count of the person at front. 
+        while (tickets[k] > 0) {
+            tickets[q.front()]--; // Decrease ticket count of the person at front. 
 
-        //     // Push back if tickets left.
-        //     if (tickets[q.front()]) {
-        //         q.push(q.front());
-        //     }
+            // Push back if tickets left.
+            if (tickets[q.front()]) {
+                q.push(q.front());
+            }
 
-        //     q.pop();  // Remove from front
-        //     time++;   // 1 second for each ticket sold.
-        // }
+            q.pop();  // Remove from front
+            time++;   // 1 second for each ticket sold.
+        }
         
-        // return time;
-//     }
-// };
+        return time;
+    }
+};
